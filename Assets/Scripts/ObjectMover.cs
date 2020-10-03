8b0b1130da7e2814a5c759a92b1927751cb4d41a
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-10-03 02:39:30 -0400

    Moving a layer around

---
+++ Assets/Scripts/ObjectMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectMover : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector2 m_Move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        m_Move = context.ReadValue<Vector2>();
+    }
+
+    public void Update()
+    {
+        Move(m_Move);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01)
+            return;
+        var move = new Vector3(direction.x, 0, direction.y);
+        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+        transform.position += move * scaledMoveSpeed;
+    }
+}
