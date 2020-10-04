df0c3111d3787e7328f2cf1429d0702b4577c55b
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-10-04 19:00:27 -0400

    Working road and turning road

--- Assets/Scripts/RoadSpawner.cs
+++ Assets/Scripts/RoadSpawner.cs
@@ -3,33 +3,19 @@ using UnityEngine;
 
 public class RoadSpawner : MonoBehaviour
 {
-    public GameObject roadPiece;
-    public GameObject roadParent;
+    public Transform roadCurve;
 
     [SerializeField]
     private float moveSpeed = 1.0f;
+    [SerializeField]
     private List<Transform> roadList;
     private Vector2 m_Move;
 
     private float layerSpawnTracker;
+    private float roadSize = 10f;
 
     void Awake()
     {
-        roadList = new List<Transform>();
-        for (int i = -10; i < 300; ++i)
-        {
-            var roadLayer = Instantiate(roadPiece) as GameObject;
-            roadLayer.transform.parent = roadParent.transform;
-            roadLayer.transform.position = new Vector3(0, 0, i);
-            roadLayer.GetComponent<ObjectMovementListener>().id = i;
-            if (i == -10)
-            {
-                roadLayer.AddComponent<RadioEvent>();
-            }
-            roadList.Add(roadParent.transform);
-        }
-
-        ObjectMovementListener.OnPassedByAction += RoadSpawner_OnPassedBy;
         ObjectMovementEvents.OnMoveAction += RoadSpawner_OnMove;
     }
 
@@ -40,46 +26,31 @@ public class RoadSpawner : MonoBehaviour
 
     private void Move(Vector2 direction)
     {
-        if (direction.sqrMagnitude < 0.01)
-            return;
-        int i = 0;
-        foreach(Transform roadPiece in roadList)
+        // if (direction.sqrMagnitude < 0.01)
+        //     return;
+
+        for(int i = 0; i < roadList.Count; i++)
         {
-            var move = new Vector3(0, 0, -direction.y);
-            var scaledMoveSpeed = moveSpeed * Time.fixedDeltaTime;
-            // var distanceToCar = Vector3.Distance(roadPiece.position, new Vector3(0, 0, -10));
-            roadPiece.position += move * scaledMoveSpeed;
-            // var roadCurveOffset = /* Vector3.Lerp( */new Vector3(0.5f * i, 0, roadPiece.position.z)/* , Vector3.zero, distanceToCar) */;
-            // roadPiece.position = roadCurveOffset;
-            i++;
+            var position = roadList[i].position;
+
+            position.z -= moveSpeed;
+
+            if (position.z < -2 * roadSize)
+            {
+                position.z += 6 * roadSize;
+            }
+
+            roadList[i].position = position;
         }
-        layerSpawnTracker += direction.y * moveSpeed * Time.fixedDeltaTime;
-        if (layerSpawnTracker > 1.0f)
-        {
-            NewRoadLayer();
-            layerSpawnTracker = 0f;
-        }
-        print(layerSpawnTracker);
+
+        var rotation = roadCurve.eulerAngles;
+        rotation.y -= 1f;
+        roadCurve.eulerAngles = rotation;
+
     }
 
     void RoadSpawner_OnMove (Vector2 direction)
     {
         m_Move = direction;
-    }
-
-    void RoadSpawner_OnPassedBy(ObjectMovementListener objectMovement)
-    {
-        // objectMovement.transform.position = new Vector3(0, 0, 300f);
-
-
-    }
-
-    void NewRoadLayer()
-    {
-        var roadLayer = Instantiate(roadPiece);
-        roadLayer.transform.parent = roadParent.transform;
-        roadLayer.transform.position = roadList.ToArray()[roadList.Count-1].transform.position + Vector3.forward;
-        roadLayer.GetComponent<ObjectMovementListener>().id = roadList.Count;
-        roadList.Add(roadParent.transform);
     }
 }
